01b22ee7f59bf40f30811c14799da83cf0e156a7
Hakan <ipekhakancan@gmail.com>
2018-07-04 23:24:36 +0300

    7.4.18 Updte

---
+++ eclipseTutorial/src/HelloWorld.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key", "value");
+
+	}
+
+}
+
+/*
+ * //Ctrl + Space //sysout System.out.println(); //syserr System.err.println();
+ * /*fore for (String string : args) {
+ * 
+ * } Ctrl + 1 => Suggestions Ctrl + Shift + R(and T) => Search for Resource or
+ * Type Ctrl + Shift + L => All shortcuts Ctrl + Shift + O => Organize Imports
+ * Ctrl + Shift + F => Formatting
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
---
+++ mavenTutorial/src/main/java/com/i2i/staj/maven/project/mavenTutorial/MavenTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.i2i.staj.maven.project.mavenTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenTutorialApplication.class, args);
+	}
+}
